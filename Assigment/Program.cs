0ae1f29e7dfc7b5434e3576dd33da57e5ad5bb1c
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-09 22:59:27 +0200

    Part 01

---
+++ Assigment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+            //Question 01 --> b) To define a blueprint for a class
+
+            //Question 02 --> a) private
+
+            //Question 03 --> c) Only if they are static
+
+            //Question 04 --> b) Yes, interfaces can inherit from multiple interfaces
+
+            //Question 05 --> d) implements
+
+            //Question 06 --> a) Yes
+
+            //Question 07 --> c) Yes, but only for abstract members
+
+            //Question 08 --> b) To provide a clear separation between interface and class members
+
+            //Question 09 --> b) No, interfaces cannot have constructors
+
+            //Question 10 --> c) By separating interface names with commas
+            #endregion
+        }
+    }
+}
